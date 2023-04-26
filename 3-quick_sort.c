831b6842b03bedc397418edad11ff185bbdd2617
NgigiN <ngigi.nyongo@gmail.com>
2023-04-26 08:44:52 +0300

    output error in 0 and 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -94,34 +94,3 @@ int partitions(int *array, int low, int high, size_t size)
 
 	return (i + 1);
 }
-/*
-int partitions(int *array, int low, int high, size_t size)
-{
-	int pivot = array[high];
-	int i = low - 1, j, temp;
-
-	for (j = low; j <= high - 1; j++)
-	{
-		if (array[j] < pivot)
-		{
-			i++;
-			if (i != j)
-			{
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				print_array(array, size);
-			}
-		}
-	}
-	if (i + 1 != high)
-	{
-
-		temp = array[i + 1];
-		array[i + 1] = array[high];
-		array[high] = temp;
-		print_array(array, size);
-	}
-	return (i + 1);
-}
-*/
---
+++ recursion.c
@@ -0,0 +1,21 @@
+#include "sort.h"
+
+void bb_sort(int *array, size_t size)
+{
+	size_t i;
+	int temp;
+
+	if(size <= 1)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		if (array[i] > array[i + 1])
+		{
+			temp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = temp;
+		}
+	}
+	bb_sort(array, size - 1);
+}	
